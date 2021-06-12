34c12559f9e4a8806a69ed1fe1960920886f19f0
Ilia Altmark <iliaaltmark@gmail.com>
2021-06-12 21:27:23 +0300

    project start - more readable

--- main_code/crawler_prototype.py
+++ main_code/crawler_prototype.py
@@ -5,21 +5,41 @@ First crawler
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.goodreads.com/choiceawards/best-books-2020"
+DOMAIN = "https://www.goodreads.com"
+URL = DOMAIN + "/choiceawards/best-books-2020"
 
 
-def main():
+def get_links_to_top_genres():
+    """
+    goes to the predefined URL and extracts the links to the top books
+    :return: a list containing the top books per genre
+    """
     user_agent = {'User-agent': 'Mozilla/5.0'}
     response1 = requests.get(URL, headers=user_agent)
 
     soup1 = BeautifulSoup(response1.content, "html.parser")
+
+    # finds the div with id="categories"
     tag = soup1.find("div", attrs={"id": "categories"})
 
+    # extracts all the links from the div
     links_to_pages = [t['href'] for t in tag.findAll("a")]
-    links_to_pages = list(filter(lambda t: True if t != '#' else False,
+
+    # cleaning empty links
+    links_to_pages = list(filter(lambda link: True if link != '#' else False,
                                  links_to_pages))
 
-    print(links_to_pages)
+    # turning into actual links and not dirs
+    links_to_pages = [DOMAIN + link for link in links_to_pages]
+
+    return links_to_pages
+
+
+def main():
+    links_to_top_genres = get_links_to_top_genres()
+
+    for link in links_to_top_genres:
+        print(link)
 
 
 if __name__ == "__main__":
