30d22f8044ec00d39a3f30f8ac0dbeb71a0a02fe
tovibenoni <tovibenoni@gmail.com>
2021-06-16 23:35:23 +0300

    added selenium HTML extracting and rating_from_link function

--- main_code/books_scraper.py
+++ main_code/books_scraper.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 from crawler_prototype import DOMAIN
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class BookRating:
 
-    def __init__(self, average_rating, number_of_reviews, rating_histogram):
+    def __init__(self, average_rating, number_of_ratings):
         self.average_rating = average_rating
-        self.number_of_reviews = number_of_reviews
-        self.rating_histogram = rating_histogram
+        self.number_of_reviews = number_of_ratings
+        # TODO:add histogram
         # TODO: assert that average_rating is indeed the average
 
 
@@ -35,9 +36,17 @@ class Book:
         """
 
     def soup_from_link(self):
-        user_agent = {'User-agent': 'Mozilla/5.0'}
-        response1 = requests.get(self.link, headers=user_agent)
-        self.soup = BeautifulSoup(response1.content, "html.parser")
+        # runs chrome, browse to the link
+        driver = webdriver.Chrome()
+        driver.get(self.link)
+
+        # clicks on the rating_details button
+        elem = driver.find_element_by_id('rating_details')
+        elem.send_keys(Keys.RETURN)
+
+        self.soup = BeautifulSoup(driver.page_source)
+        driver.close()
+
 
     def _name_from_link(self):
         if self.soup is None:
@@ -59,3 +68,19 @@ class Book:
         tag = self.soup.find("div", attrs={"id": "description"})
         description = tag.text.strip()
         self.description = description
+
+    def _rating_from_link(self):
+        if self.soup is None:
+            self.soup_from_link()
+        tag = self.soup.find("span", attrs={"itemprop": "ratingValue"})
+        rating = int(tag.text.strip())
+        tag2 = self.soup.find("meta", attrs={"itemprop": "ratingCount"})
+        num_ratings = int(tag2.attrs["content"])
+        self.rating=BookRating(average_rating=rating,number_of_ratings=num_ratings)
+
+    def _genres_from_link(self):
+        if self.soup is None:
+            self.soup_from_link()
+        tag = self.soup.find("div", attrs={"class": "bigBoxContent containerWithHeaderContent"})
+        genres_dict={}
+
