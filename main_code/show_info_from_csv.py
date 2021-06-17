a91a07b953c1d3ccf076b7eed531752954ba1c01
Ilia Altmark <iliaaltmark@gmail.com>
2021-06-17 20:54:34 +0300

    creates a new csv with book info

--- main_code/show_info_from_csv.py
+++ main_code/show_info_from_csv.py
@@ -10,13 +10,15 @@ URL = DOMAIN + "/choiceawards/best-books-2020"
 
 
 def main():
-    book_dict = {'name': [], 'author': [], 'description': [], 'top_of': []}
+    book_dict = {'name': [], 'author': [], 'description': [],
+                 'average_rating': [], 'number_of_reviews': [], 'top_of': []}
 
     with open('links_to_books.csv', newline='') as csv_file:
         reader = csv.reader(csv_file)
 
         for i, row in enumerate(reader):
-            if i >= 10:
+            print(f"Printing row number {i}...")
+            if i >= 1:
                 break
 
             link = row[0]
@@ -27,6 +29,10 @@ def main():
                 book.author.encode("utf-8", errors='replace'))
             book_dict['description'].append(
                 book.description.encode("utf-8", errors='replace'))
+            book_dict['average_rating'].append(
+                book.rating.average_rating)
+            book_dict['number_of_reviews'].append(
+                book.rating.number_of_reviews)
             book_dict['top_of'].append(row[1])
 
     with open('books.csv', 'w', newline='') as csv_file:
