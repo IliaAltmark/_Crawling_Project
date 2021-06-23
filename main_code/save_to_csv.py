be47ec95ce792438e0e8896cc371bfd8dcbd12e3
Ilia Altmark <iliaaltmark@gmail.com>
2021-06-23 13:39:40 +0300

    final changes

--- main_code/save_to_csv.py
+++ main_code/save_to_csv.py
@@ -12,22 +12,15 @@ from book_scraper import Book
 
 FROM_ROW = 0
 TO_ROW = 400
+# READING_FROM = '../project_data/links_to_books.csv'
 READING_FROM = '../project_data/links_to_books_test.csv'
 WRITING_TO = '../project_data/books_test.csv'
 
 
-def main():
-    # declaring a dictionary which will contain lists of scraped data.
-    # each list contains data scraped from the same place in the HTML
-    book_dict = {'name': [], 'author': [], 'description': [],
-                 'average_rating': [], 'number_of_reviews': [],
-                 'top_of': [],
-                 'rated_5': [], 'rated_4': [], 'rated_3': [], 'rated_2': [],
-                 'rated_1': [],
-                 'top_voted_genre': [], 'top_voted_votes': [],
-                 '2nd_voted_genre': [], '2nd_voted_votes': [],
-                 '3rd_voted_genre': [], '3rd_voted_votes': []}
-
+def create_dict(b_dict):
+    """
+    fills up the given dictionary with scraped data
+    """
     # reading from a file containing links to books
     with open(READING_FROM, newline='') as csv_file:
         reader = csv.reader(csv_file)
@@ -50,65 +43,86 @@ def main():
                         break
 
                     # saving the scraped data in the dictionary
-                    book_dict['name'].append(
+                    b_dict['name'].append(
                         book.name)
-                    book_dict['author'].append(
+                    b_dict['author'].append(
                         book.author)
-                    book_dict['description'].append(
+                    b_dict['description'].append(
                         book.description)
-                    book_dict['average_rating'].append(
+                    b_dict['average_rating'].append(
                         book.rating.average_rating)
-                    book_dict['number_of_reviews'].append(
+                    b_dict['number_of_reviews'].append(
                         book.rating.number_of_reviews)
-                    book_dict['top_of'].append(row[1])
-                    book_dict['rated_5'].append(
+                    b_dict['top_of'].append(row[1])
+                    b_dict['rated_5'].append(
                         book.rating.rating_histogram[5])
-                    book_dict['rated_4'].append(
+                    b_dict['rated_4'].append(
                         book.rating.rating_histogram[4])
-                    book_dict['rated_3'].append(
+                    b_dict['rated_3'].append(
                         book.rating.rating_histogram[3])
-                    book_dict['rated_2'].append(
+                    b_dict['rated_2'].append(
                         book.rating.rating_histogram[2])
-                    book_dict['rated_1'].append(
+                    b_dict['rated_1'].append(
                         book.rating.rating_histogram[1])
 
                     genres = iter(book.genres)
 
                     top_voted_key = next(genres)
-                    book_dict['top_voted_genre'].append(top_voted_key[0])
-                    book_dict['top_voted_votes'].append(
+                    b_dict['top_voted_genre'].append(top_voted_key[0])
+                    b_dict['top_voted_votes'].append(
                         book.genres[top_voted_key])
 
                     try:
                         second_voted_key = next(genres)
-                        book_dict['2nd_voted_genre'].append(
+                        b_dict['2nd_voted_genre'].append(
                             second_voted_key[0])
-                        book_dict['2nd_voted_votes'].append(
+                        b_dict['2nd_voted_votes'].append(
                             book.genres[second_voted_key])
                     except StopIteration:
-                        book_dict['2nd_voted_genre'].append('None')
-                        book_dict['2nd_voted_votes'].append('None')
+                        b_dict['2nd_voted_genre'].append('None')
+                        b_dict['2nd_voted_votes'].append('None')
 
                     try:
                         third_voted_key = next(genres)
-                        book_dict['3rd_voted_genre'].append(third_voted_key[0])
-                        book_dict['3rd_voted_votes'].append(
+                        b_dict['3rd_voted_genre'].append(third_voted_key[0])
+                        b_dict['3rd_voted_votes'].append(
                             book.genres[third_voted_key])
                     except StopIteration:
-                        book_dict['3rd_voted_genre'].append('None')
-                        book_dict['3rd_voted_votes'].append('None')
+                        b_dict['3rd_voted_genre'].append('None')
+                        b_dict['3rd_voted_votes'].append('None')
 
         finally:
             driver.close()
 
-    # opens or creates the file and saves all the scraped book info in
-    # the csv
-    with open(WRITING_TO, 'a', newline='',
-              encoding='utf-8') as csv_file:
 
+def create_csv(b_dict):
+    """
+    saves the scraped data from the given dictionary to a csv
+    """
+    with open(WRITING_TO, 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(book_dict.keys())
-        writer.writerows(zip(*book_dict.values()))
+        writer.writerow(b_dict.keys())
+        writer.writerows(zip(*b_dict.values()))
+
+
+def main():
+    # declaring a dictionary which will contain lists of scraped data.
+    # each list contains data scraped from the same place in the HTML
+    book_dict = {'name': [], 'author': [], 'description': [],
+                 'average_rating': [], 'number_of_reviews': [],
+                 'top_of': [],
+                 'rated_5': [], 'rated_4': [], 'rated_3': [], 'rated_2': [],
+                 'rated_1': [],
+                 'top_voted_genre': [], 'top_voted_votes': [],
+                 '2nd_voted_genre': [], '2nd_voted_votes': [],
+                 '3rd_voted_genre': [], '3rd_voted_votes': []}
+
+    try:
+        create_dict(book_dict)
+    except FileNotFoundError:
+        print('The file address is incorrect!')
+    else:
+        create_csv(book_dict)
 
 
 if __name__ == "__main__":
