baafec6be301b24e1629cb8c36f6c75c3748e74a
tovibenoni <tovibenoni@gmail.com>
2021-07-08 22:58:57 +0300

    added tables in tables_setup

--- database_setup/tables_setup.py
+++ database_setup/tables_setup.py
@@ -9,9 +9,11 @@ from utils.sql_utils import establish_connection, sql_run
 def main():
     # establish connection to SQL
     connection = establish_connection()
+
     # create the good_reads_data database
     sql_run(connection, "CREATE DATABASE IF NOT EXISTS good_reads_data;")
     connection.select_db("good_reads_data")
+
     # books table
     # remove the table books if it exists
     sql_run(connection, "DROP TABLE IF EXISTS books;")
@@ -24,22 +26,38 @@ def main():
                         "top_voted_genre VARCHAR(255), top_voted_votes INT,2nd_voted_genre VARCHAR(255), "
                         "2nd_voted_votes "
                         "INT,3nd_voted_genre VARCHAR(255), 3nd_voted_votes INT);")
+
     # description table
     # remove the table description if it exists
     sql_run(connection, "DROP TABLE IF EXISTS description;")
     # create the description table
     sql_run(connection,
             "CREATE TABLE IF NOT EXISTS description (book_number INT PRIMARY KEY, description "
-            "VARCHAR(4095));")
+            "TEXT(4095);")
 
-    # category table
-    # remove the table category if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS category;")
-    # create the category table
-    sql_run(connection, "CREATE TABLE IF NOT EXISTS category (top_of_category VARCHAR(255) PRIMARY KEY, "
-                        "link VARCHAR(255));")
+    # books_genre table
+    # remove the table books_genre if it exists
+    sql_run(connection, "DROP TABLE IF EXISTS books_genre;")
+    # create the books_genre table
+    sql_run(connection, "CREATE TABLE IF NOT EXISTS books_genre (book_id INT, genre_id INT, top_voted INT, "
+                        "top_voted_num INT);")
 
-    connection.close()
+    # genre table
+    # remove the table genre if it exists
+    sql_run(connection, "DROP TABLE IF EXISTS genre;")
+    # create the genre table
+    sql_run(connection, "CREATE TABLE IF NOT EXISTS genre (genre_id INT AUTO_INCREMENT PRIMARY KEY, genre VARCHAR("
+                        "255));")
+
+    # rating_info table
+    # remove the table rating_info if it exists
+    sql_run(connection, "DROP TABLE IF EXISTS rating_info;")
+    # create the rating_info table
+    sql_run(connection, "CREATE TABLE IF NOT EXISTS rating_info (book_number INT PRIMARY KEY rated_5 INT, "
+                        "rated_4 INT,rated_3 INT,rated_2 INT, "
+                        "rated_1 INT;")
+
+connection.close()
 
 
 if __name__ == "__main__":
