d089d18a40e35c332c00599e2e3c995bd7d1dc4d
Ilia Altmark <iliaaltmark@gmail.com>
2021-07-10 14:24:13 +0300

    adding FK to database creation script

--- database_setup/tables_setup.py
+++ database_setup/tables_setup.py
@@ -8,73 +8,83 @@ def main():
     # establish connection to SQL
     connection = establish_connection()
 
+    # remove the database Good_Reads_Data if it exists
+    sql_run(connection, "DROP database IF EXISTS Good_Reads_Data;")
     # create the good_reads_data database
-    print('Creating good_reads_data database...', end='')
-    sql_run(connection, "CREATE DATABASE IF NOT EXISTS good_reads_data")
-    connection.select_db("good_reads_data")
+    print('Creating Good_Reads_Data database...', end='')
+    sql_run(connection, "CREATE DATABASE Good_Reads_Data")
+    connection.select_db("Good_Reads_Data")
     print('Done.')
 
     # books table
-    # remove the table books if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS books;")
     # create the books table
-    print('Creating books table...', end='')
-    sql_run(connection, """CREATE TABLE IF NOT EXISTS 
-                           books (
-                           book_number INT AUTO_INCREMENT PRIMARY KEY, 
-                           book_link VARCHAR(255), 
-                           best_of VARCHAR(255), 
-                           title VARCHAR(255), 
-                           author VARCHAR(255), 
-                           average_rating FLOAT, 
-                           number_of_reviews INT
+    print('Creating Books table...', end='')
+    # TODO remove books_link: done
+    sql_run(connection, """CREATE TABLE
+                           Books (
+                               book_id INT AUTO_INCREMENT PRIMARY KEY, 
+                               best_of VARCHAR(255), 
+                               title VARCHAR(255), 
+                               author VARCHAR(255), 
+                               average_rating FLOAT, 
+                               number_of_reviews INT
                            );""")
     print('Done.')
 
     # description table
-    # remove the table description if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS description;")
     # create the description table
-    print('Creating description table...', end='')
-    sql_run(connection, """CREATE TABLE IF NOT EXISTS 
-                           description (
-                           book_number INT PRIMARY KEY, 
-                           description TEXT(4095)
-                           );""")
-    print('Done.')
-
-    # books_genre table
-    # remove the table books_genre if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS books_genre;")
-    # create the books_genre table
-    print('Creating books_genre table...', end='')
-    sql_run(connection, """CREATE TABLE IF NOT EXISTS 
-                           books_genre (
-                           book_id INT, 
-                           genre VARCHAR(255), 
-                           top_voted INT, 
-                           top_voted_num INT
+    print('Creating Description table...', end='')
+    sql_run(connection, """CREATE TABLE 
+                           Description (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               book_id INT, 
+                               description TEXT(4095),
+                               FOREIGN KEY (book_id) 
+                               REFERENCES Books(book_id)
                            );""")
     print('Done.')
 
     # genre table
-    # remove the table genre if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS genre;")
     # create the genre table
-    print('Creating genre table...', end='')
-    sql_run(connection,
-            "CREATE TABLE IF NOT EXISTS genre (genre_id INT AUTO_INCREMENT PRIMARY KEY, genre VARCHAR("
-            "255));")
+    print('Creating Genre table...', end='')
+    sql_run(connection, """CREATE TABLE 
+                               Genre ( 
+                                    genre VARCHAR(255) PRIMARY KEY
+                               );""")
+    print('Done.')
+
+    # books_genre table
+    # create the books_genre table
+    print('Creating Books_Genre table...', end='')
+    sql_run(connection, """CREATE TABLE 
+                           Books_Genre (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               book_id INT, 
+                               genre VARCHAR(255), 
+                               top_voted INT, 
+                               top_voted_num INT,
+                               FOREIGN KEY (book_id) 
+                               REFERENCES Books(book_id),
+                               FOREIGN KEY (genre) 
+                               REFERENCES Genre(genre)
+                           );""")
     print('Done.')
 
     # rating_info table
-    # remove the table rating_info if it exists
-    sql_run(connection, "DROP TABLE IF EXISTS rating_info;")
     # create the rating_info table
-    print('Creating rating_info table...', end='')
-    sql_run(connection,
-            "CREATE TABLE IF NOT EXISTS rating_info (book_number INT PRIMARY KEY, rated_5 INT, "
-            "rated_4 INT,rated_3 INT,rated_2 INT, rated_1 INT);")
+    print('Creating Rating_Info table...', end='')
+    sql_run(connection, """CREATE TABLE
+                           Rating_Info (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               book_id INT, 
+                               rated_5 INT, 
+                               rated_4 INT, 
+                               rated_3 INT, 
+                               rated_2 INT, 
+                               rated_1 INT,
+                               FOREIGN KEY (book_id) 
+                               REFERENCES Books(book_id)
+                           );""")
     print('Done.')
 
     connection.close()
